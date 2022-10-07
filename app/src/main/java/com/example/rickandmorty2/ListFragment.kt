072ec42d268654bf23b2853f362b6cd5c67eba30
Ewelyn <ewelyn.silva@picpay.com>
2022-10-07 16:11:54 -0300

    otimização do build.grandle e exclusão de arquivos não utilizados

--- app/src/main/java/com/example/rickandmorty2/ListFragment.kt
+++ app/src/main/java/com/example/rickandmorty2/ListFragment.kt
@@ -1,10 +1,8 @@
 package com.example.rickandmorty2
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_list.*
 
